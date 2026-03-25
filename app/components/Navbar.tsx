99b5d3d251c7661dc0c23240f112c55e5b619b15
kryderman-prog <kryderman-prog@github.com>
2026-03-25 15:45:46 +0530

    commit 24

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import type { User } from "@supabase/supabase-js";
 import { LogoutButton } from "./LogoutButton";
 
+
+
 interface Profile {
   id: string;
   name: string;
